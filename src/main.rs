eccf51101fa14cd0ebe935179ab4da7850c4a93d
Olga Telezhnaya <olyatelezhnaya@gmail.com>
2022-02-18 15:51:53 +0300

    update the project to latest near-lake-framework version

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use futures::StreamExt;
+use serde_json::json;
 
 use near_lake_framework::LakeConfig;
 
 #[tokio::main]
 async fn main() -> Result<(), tokio::io::Error> {
     let config = LakeConfig {
-        bucket: "near-lake-testnet".to_string(),
-        region: "eu-central-1".to_string(),
-        start_block_height: None, // want to start from the freshest
-        tracked_shards: vec![],
+        s3_bucket_name: "near-lake-testnet".to_string(),
+        start_block_height: 82422587,
+        s3_region_name: "eu-central-1".to_string(),
     };
     let stream = near_lake_framework::streamer(config);
 
     let mut handlers = tokio_stream::wrappers::ReceiverStream::new(stream)
-        .map(|json_value| handle_json_value(json_value))
+        .map(|streamer_message| handle_json_value(json!(streamer_message)))
         .buffer_unordered(1usize);
 
     while let Some(_handle_message) = handlers.next().await {}
@@ -21,7 +21,10 @@ async fn main() -> Result<(), tokio::io::Error> {
     Ok(())
 }
 
-
 async fn handle_json_value(json_value: serde_json::Value) {
-    eprintln!("{} / shards {}", json_value["block"]["header"]["height"], json_value["shards"].as_array().unwrap().len());
+    eprintln!(
+        "{} / shards {}",
+        json_value["block"]["header"]["height"],
+        json_value["shards"].as_array().unwrap().len()
+    );
 }
