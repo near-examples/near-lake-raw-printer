30a26f01c9ea74fd5ec766a6bbdb8dd73feea5eb
Bohdan Khorolets <bogdan@khorolets.com>
2022-02-18 15:15:24 +0200

    Renaming, update to latest near-lake-framework

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use futures::StreamExt;
+use tracing_subscriber::EnvFilter;
 
 use near_lake_framework::LakeConfig;
 
 #[tokio::main]
 async fn main() -> Result<(), tokio::io::Error> {
+    init_tracing();
+
     let config = LakeConfig {
-        bucket: "near-lake-testnet".to_string(),
-        region: "eu-central-1".to_string(),
-        start_block_height: None, // want to start from the freshest
-        tracked_shards: vec![],
+        s3_bucket_name: "near-lake-testnet".to_string(),
+        s3_region_name: "eu-central-1".to_string(),
+        start_block_height: 83030086, // want to start from the freshest
     };
     let stream = near_lake_framework::streamer(config);
 
     let mut handlers = tokio_stream::wrappers::ReceiverStream::new(stream)
-        .map(|json_value| handle_json_value(json_value))
+        .map(|streamer_message| handle_streamer_message(streamer_message))
         .buffer_unordered(1usize);
 
     while let Some(_handle_message) = handlers.next().await {}
@@ -21,7 +23,38 @@ async fn main() -> Result<(), tokio::io::Error> {
     Ok(())
 }
 
+async fn handle_streamer_message(
+    streamer_message: near_lake_framework::near_indexer_primitives::StreamerMessage,
+) {
+    // eprintln!("{:#?}", &streamer_message);
+    eprintln!(
+        "{} / shards {}",
+        streamer_message.block.header.height,
+        streamer_message.shards.len()
+    );
+}
 
-async fn handle_json_value(json_value: serde_json::Value) {
-    eprintln!("{} / shards {}", json_value["block"]["header"]["height"], json_value["shards"].as_array().unwrap().len());
+fn init_tracing() {
+    let mut env_filter = EnvFilter::new(
+        "near_lake_framework=info",
+    );
+
+    if let Ok(rust_log) = std::env::var("RUST_LOG") {
+        if !rust_log.is_empty() {
+            for directive in rust_log.split(',').filter_map(|s| match s.parse() {
+                Ok(directive) => Some(directive),
+                Err(err) => {
+                    eprintln!("Ignoring directive `{}`: {}", s, err);
+                    None
+                }
+            }) {
+                env_filter = env_filter.add_directive(directive);
+            }
+        }
+    }
+
+    tracing_subscriber::fmt::Subscriber::builder()
+        .with_env_filter(env_filter)
+        .with_writer(std::io::stderr)
+        .init();
 }
